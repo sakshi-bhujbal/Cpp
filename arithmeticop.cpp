1a95d93d59f40ca4ff69dd1e814e2e1b691412e6
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-05-28 19:59:08 +0530

    commit

---
+++ arithmeticop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the first number"<<endl;
+    cin>>a;
+
+
+    cout<<"Enter the second number"<<endl;
+    cin>>b;
+
+    cout<<"The sum of a and b is "<<a+b<<endl;//gives addition
+    cout<<"The difference of a and b is :"<<a-b<<endl;//gives the subtraction
+    cout<<"The division of a and b is "<<a/b<<endl;//gives integer division
+    cout<<"The mod of a and b is "<<a%b<<endl;//gives remainder
+}
