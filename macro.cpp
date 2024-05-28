8858503222d595dd5472f610c1ca75a6c35cb6c9
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-05-28 18:46:02 +0530

    commit

---
+++ macro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#define LIMIT  5//macro definition
+using namespace std;
+int main(){
+    for(int i=0;i<LIMIT;i++)
+    {
+        cout<<i<<endl;
+       
+    }
+     return 0;
+}
