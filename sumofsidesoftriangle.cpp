0ddc9a89547256644d217da23c077c97a577edf8
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-05-29 20:46:35 +0530

    commit

---
+++ sumofsidesoftriangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the sides of triangle: ";
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    if(a+b>c || b+c>a || c+a>b)
+    {
+        cout<<a<<","<<b<<","<<c<<" are the sides of triangle";
+    }
+    else{
+        cout<<"are not the sides of triangle";
+    }
+}
