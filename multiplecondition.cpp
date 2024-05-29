046b6fc2fb027bc3cc001c053a9c616f91f77a2d
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-05-29 20:30:56 +0530

    commit

---
+++ multiplecondition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cout<<"Enter a positive integer : ";
+cin>>x;
+if(x>99 && x<1000)
+{
+    cout<<"the number is a three digit number";
+}
+else{
+    cout<<"invalid integer";
+}
+}
+       
