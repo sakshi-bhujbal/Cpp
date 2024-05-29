53f185821449ae6a2dbdc88a59c3b70e89dd6617
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-05-29 15:07:47 +0530

    commit

---
+++ areaofcircle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r , pi=3.14;
+    cout<<"Enter radius of circle"<<endl;
+    cin>>r;
+    float area = pi*r*r;
+    cout<<"The area of circle is:"<<area<<endl;
+}
