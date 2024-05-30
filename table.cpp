af0bcf076bd9e3457a5750e7c1df3703197ea038
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-05-30 15:21:11 +0530

    commit

--- table.cpp
+++ table.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 int main(){
 
-    for(int i=1;i<=10;i++){
-        cout<<"2*"<<i<<"="<<2*i<<endl;
-    }
+    // for(int i=1;i<=10;i++){
+    //     cout<<"19*"<<i<<"="<<19*i<<endl;
+    // }
+    // for(int i=19;i<=190;i+=19)
+    // cout<<i<<" ";
+
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    cout<<n<<"*"<<i<<"="<<n*i<<endl;
+
 }
