63742761c233e372fc50b4db3559f96a2295f92f
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-05-30 14:46:39 +0530

    commit

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+/*int main(){
+    int x=3,y=5;
+    if(x==3)
+        cout<<x<<endl;
+    else;
+        cout<<y<<endl;
+}*/
+int main() {
+int x = 3, y, z;
+y = x = 10;
+z = x < 10;
+cout<<x<<" "<<y<<" "<<z;
+}
