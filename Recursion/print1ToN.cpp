7d45c30864196cbd63845db26061b210956cbb40
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-03 19:16:07 +0530

    commit

--- Recursion/print1ToN.cpp
+++ Recursion/print1ToN.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
 using namespace std;
-void print(int x,int n){
-    if(x>n) 
+// void print(int x,int n){
+//     if(x>n) 
+//     return;
+//     cout<<x<<endl;
+//     print(x+1,n);
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter n: ";
+//     cin>>n;
+//     print(1,n);
+// }
+#include<iostream>
+using namespace std;
+void print(int n){
+    if(n==0) 
     return;
-    cout<<x<<endl;
-    print(x+1,n);
+    
+    print(n-1);
+    cout<<n<<endl;
 }
 int main(){
-    int n;
-    cout<<"Enter n: ";
-    cin>>n;
-    print(1,n);
+    print(5);
 }
