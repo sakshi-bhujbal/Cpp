31933fd9f2fede93519fa6dc2605eab59d19e408
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-05 23:14:40 +0530

    commit

---
+++ Array/secondLargest.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={3,6,4,8,9};
+    int n=sizeof(arr)/4;
+    int mx=INT_MIN;
+
+    for(int i=0;i<n;i++){
+        mx=max(mx,arr[i]);
+    }
+    int secmx=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=mx)
+        secmx=max(secmx,arr[i]);
+    }
+    cout<<secmx;
+}
