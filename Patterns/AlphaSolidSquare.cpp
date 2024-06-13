99a38ded09f18ddab5020220f3f794a142715a8b
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-13 17:16:44 +0530

    commit

--- Patterns/AlphaSolidSquare.cpp
+++ Patterns/AlphaSolidSquare.cpp
@@ -1,3 +1,39 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int m;
+//     cout<<"Enter the value of m: ";
+//     cin>>m;
+    
+//     for(int i=1;i<=m;i++){
+        
+//         for(int j=1;j<=m;j++)
+//         {
+//             cout<<(char)(i+64);
+            
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int m;
+//     cout<<"Enter the value of m: ";
+//     cin>>m;
+//     int count=1;
+//     for(int i=1;i<=m;i++){
+        
+//         for(int j=1;j<=m;j++)
+//         {
+//             cout<<(char)(count+64);
+//             count++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -6,10 +42,11 @@ int main(){
     cin>>m;
     
     for(int i=1;i<=m;i++){
-        
+        int n=i;
         for(int j=1;j<=m;j++)
         {
-            cout<<(char)(i+64);
+            cout<<(char)(n+64);
+            n++;
             
         }
         cout<<endl;
