637d2a8c5c75083feeaf32b0d68a57651b10ff32
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-13 13:29:54 +0530

    commit

--- Patterns/triangleStartingfromRowNum.cpp
+++ Patterns/triangleStartingfromRowNum.cpp
@@ -1,3 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int m;
+//     cout<<"Enter the value of m: ";
+//     cin>>m;
+    
+//     for(int i=1;i<=m;i++){
+//         int n=i;
+//         for(int j=1;j<=i;j++)
+//         {
+//             cout<<n;
+//             n++;   
+//         }
+//         cout<<endl;
+//     }
+// }
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,7 +27,7 @@ int main(){
         for(int j=1;j<=i;j++)
         {
             cout<<n;
-            n++;
+            n=n-1;
         }
         cout<<endl;
     }
