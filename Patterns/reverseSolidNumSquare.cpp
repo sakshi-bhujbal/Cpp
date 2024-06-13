a10036106ef3fd9165c8849486c5e393697d7bf9
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-13 13:14:59 +0530

    commit

--- Patterns/reverseSolidNumSquare.cpp
+++ Patterns/reverseSolidNumSquare.cpp
@@ -20,16 +20,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=1;
     int m;
     cout<<"Enter the value of m: ";
     cin>>m;
     
     for(int i=1;i<=m;i++){
+        int n=i;
         for(int j=1;j<=i;j++)
         {
-            cout<<i;
-            
+            cout<<n;
+            n++;
         }
         cout<<endl;
     }
