1606da410d14dfe6f2ce1826f19be3b3ad5efa4a
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-13 17:37:42 +0530

    commit

--- Patterns/alpha2Triangle.cpp
+++ Patterns/alpha2Triangle.cpp
@@ -1,3 +1,40 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,j;
+//     int m;
+//     cout<<"Enter the value of m: ";
+//     cin>>m;
+    
+//     for(i=1;i<=4;i++){
+//         for(j=1;j<=i;j++)
+//         {
+//             cout<<(char)(j+64);
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,j;
+//     int m;
+//     cout<<"Enter the value of m: ";
+//     cin>>m;
+    
+//     for(i=1;i<=m;i++){
+//         int n=i;
+//         for(j=1;j<=i;j++)
+//         {
+//             cout<<(char)(n+64);
+//             n++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
@@ -5,11 +42,12 @@ int main(){
     int m;
     cout<<"Enter the value of m: ";
     cin>>m;
-    
-    for(i=1;i<=4;i++){
+    int count=1;
+    for(i=1;i<=m;i++){
         for(j=1;j<=i;j++)
         {
-            cout<<(char)(j+64);
+            cout<<(char)((count+64)+m-i);
+            
         }
         cout<<endl;
     }
