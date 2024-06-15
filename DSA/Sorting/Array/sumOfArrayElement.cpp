5af9a25175546e1185e9a278bcd90f0be3227fed
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-15 13:36:59 +0530

    commit

---
+++ DSA/Sorting/Array/sumOfArrayElement.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+sumOfArrayElement(int arr[],int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+    cout<<"The sum of array elements is: "<<sum<<endl;
+}
+
+
+int main(){
+    int arr[5]={4,9,5,-1,0};
+    int n=sizeof(arr)/4;
+    sumOfArrayElement(arr,5);
+}
