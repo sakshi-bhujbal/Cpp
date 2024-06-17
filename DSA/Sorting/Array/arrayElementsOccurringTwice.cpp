19b7bc50fe387908df17a768435ed7537e7c314a
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2024-06-17 20:45:09 +0530

    commit

--- DSA/Sorting/Array/arrayElementsOccurringTwice.cpp
+++ DSA/Sorting/Array/arrayElementsOccurringTwice.cpp
@@ -1,1 +1,20 @@
-#include
+#include<iostream>
+using namespace std;
+
+void twice(int arr[],int size){
+    cout<<"The number occurred twice is: ";
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            if(arr[i]==arr[j]){
+                cout<< arr[i]<<" ";//number occurred twice
+            }
+        }
+    }
+}
+
+int main(){
+int arr[8]={4,3,2,7,8,2,3,1};
+
+twice(arr,8);
+
+}
